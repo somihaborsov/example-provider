75e829d75a2e66ef2ecfacba47c52b03ef91f65d
Alexander <sasha133044@gmail.com>
2023-10-22 21:35:51 +0300

    SOB-000 Add product colors

--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,9 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", "green")],
             ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", "red")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", "yellow")],
         ]);
     }
 
